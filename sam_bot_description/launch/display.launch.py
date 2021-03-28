2a5b8780d6d912c6ee182f4b60a07e96fc4d3148
areeya01 <aurubenecia@gmail.com>
2021-03-29 07:53:40 +0800

    Add files via upload

--- sam_bot_description/launch/display.launch.py
+++ sam_bot_description/launch/display.launch.py
@@ -7,14 +7,13 @@ def generate_launch_description():
     pkg_share = launch_ros.substitutions.FindPackageShare(package='sam_bot_description').find('sam_bot_description')
     default_model_path = os.path.join(pkg_share, 'src/description/sam_bot_description.urdf')
     default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
-    
+
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        name='robot_state_publisher',
-        output='screen',
-        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])},{'use_sim_time': True}]
+        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
     )
+
     joint_state_publisher_node = launch_ros.actions.Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
@@ -34,20 +33,24 @@ def generate_launch_description():
         output='screen',
         arguments=['-d', LaunchConfiguration('rvizconfig')],
     )
+    
+    spawn_entity = launch_ros.actions.Node(
+    	package='gazebo_ros', 
+    	executable='spawn_entity.py',
+        arguments=['-entity', 'sam_bot', '-topic', 'robot_description'],
+        output='screen'
+    )
+    
     robot_localization_node = launch_ros.actions.Node(
          package='robot_localization',
          executable='ekf_node',
          name='ekf_filter_node',
          output='screen',
-         parameters=[os.path.join(pkg_share, 'config/efk.yaml')],
+         parameters=[os.path.join(pkg_share, 'config/ekf.yaml')],
     )
-    odom_publisher_node = launch_ros.actions.Node(
-         package='sam_bot_description',
-         executable='odom_publisher',
-         name='odom_publisher',
-         output='screen',
-    )
-
+    
+    
+    
     return launch.LaunchDescription([
         launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
                                             description='Flag to enable joint_state_publisher_gui'),
@@ -55,10 +58,16 @@ def generate_launch_description():
                                             description='Absolute path to robot urdf file'),
         launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
                                             description='Absolute path to rviz config file'),
+            
+        launch.actions.ExecuteProcess(
+            cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so'],
+            output='screen'),
+     
         joint_state_publisher_node,
-        joint_state_publisher_gui_node,
+        #joint_state_publisher_gui_node,
         robot_state_publisher_node,
-        rviz_node,
+        spawn_entity,
         robot_localization_node,
-        odom_publisher_node
+        rviz_node
+        
     ])
